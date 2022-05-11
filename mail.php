e7239d28cd9522b6a42b003c856b5b20249ad124
Anton Gorokhov <tony_harbour@mail.ru>
2022-05-12 01:23:47 +0900

    Update mail.php

--- mail.php
+++ mail.php
@@ -13,12 +13,12 @@ $message = $_POST['message'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'testdlyasaytov1@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'qwdqwdqdqwd'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'tony_harbour@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'privetot_tony01'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('qwdqwdqdqwd'); // от кого будет уходить письмо?
+$mail->setFrom('tony_harbour@mail.ru'); // от кого будет уходить письмо?
 $mail->addAddress('testdlyasaytov@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
